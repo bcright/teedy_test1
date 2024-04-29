86a330b7a86c58ffd98f5f42588cc104f26e14a9
12111043 <1256514466@qq.com>
2024-04-29 17:54:37 +0800

    all

--- docs-core/src/test/java/com/sismics/util/TestImageUtil.java
+++ docs-core/src/test/java/com/sismics/util/TestImageUtil.java
@@ -2,6 +2,7 @@ package com.sismics.util;
 
 import org.junit.Assert;
 import org.junit.Test;
+import java.awt.image.BufferedImage;
 
 /**
  * Test of the image utilities.
@@ -13,5 +14,19 @@ public class TestImageUtil {
     @Test
     public void computeGravatarTest() {
         Assert.assertEquals("0bc83cb571cd1c50ba6f3e8a78ef1346", ImageUtil.computeGravatar("MyEmailAddress@example.com "));
+    }
+
+    @Test
+    public void testIsBlack() {
+        BufferedImage image = new BufferedImage(10, 10, BufferedImage.TYPE_INT_RGB);
+
+        // Test outside image boundaries
+        Assert.assertFalse(ImageUtil.isBlack(image, -1, 5));
+        Assert.assertFalse(ImageUtil.isBlack(image, 5, -1));
+        Assert.assertFalse(ImageUtil.isBlack(image, 15, 5));
+        Assert.assertFalse(ImageUtil.isBlack(image, 5, 15));
+
+        // Test inside image boundaries
+        Assert.assertTrue(ImageUtil.isBlack(image, 5, 5));
     }
 }
